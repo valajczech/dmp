b65abb7f44dbffd87284310e15be6345b46e36e8
valajczech <jiri.vala11@gmail.com>
2021-04-15 00:50:59 +0200

    firebase init apparentely

---
+++ website/webpack.prod.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const buildPath = path.resolve(__dirname, 'dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+	entry: {
+		main: './src/js/main.js',
+		index: './src/js/index.js',
+		bio: './src/js/bio.js'
+	},
+
+	devServer: {
+		port: 5500,
+		contentBase: buildPath
+	},
+
+	module: {
+		rules: [
+			// load JS
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			},
+			// load CSS
+			{
+				test: /\.css$/,
+				use: [
+				"style-loader",
+				"css-loader"
+				]
+			},
+			// load images (base64 < 8192B)
+			{
+				test: /\.(png|jpg|gif|webp)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: 'img/[name][hash].[ext]'
+				}
+			},
+			// load icons
+			{
+				test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+				type: 'asset/resource'
+			}
+		]
+	},
+
+	plugins: [
+		/* INDEX */
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			inject: true,
+			chunks: ['main', 'index'],
+			filename: 'index.html',
+      minify: {
+				removeRedundantAttributes: false,
+			}
+		}),
+		/* BIOGRAPHY */
+		new HtmlWebpackPlugin({
+			template: './src/bio.html',
+			inject: true,
+			chunks: ['main', 'bio'],
+			filename: 'bio.html',
+      minify: {
+				removeRedundantAttributes: false,
+			}
+		})
+	]
+};
