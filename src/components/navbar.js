147e7344841a0855c5bcabb077c9b9b3e7896922
valajczech <jiri.vala11@gmail.com>
2021-04-15 23:02:04 +0200

    Basic respo done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,36 @@ class Navbar extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-    <a href="/">    <img src="http://www.milanbures.cz/frontend/html/img/logo_cerna.png"></img
+    <div>
+    <a href="/">
+      <img
+        src="http://www.milanbures.cz/frontend/html/img/logo_cerna.png"
+        alt=""
+      />
     </a>
+  </div>
+  <div id="burger">
+  <span id="right"></span>
+  <span id="midline"></span>
+  <span id="left"></span>
+  </div>
+  <menu-wrapper id="sliderMenu"></menu-wrapper>
     `;
+
+    const burgerButton = document.querySelector("#burger");
+    const slider = document.querySelector('#sliderMenu')
+    const mainContent = document.querySelector('.main');
+
+    burgerButton.addEventListener("click", () => {
+      burgerButton.classList.toggle('open');
+      slider.classList.toggle('slider-shown');
+     //mainContent.classList.toggle('blurred')
+    });
+
+    document.addEventListener('scroll', () => {
+      slider.classList.remove('slider-shown');
+      burgerButton.classList.remove('open')
+    })
   }
 }
 
