e47b1681511ef000af95bf87bf3b7ca75a4136e0
valajczech <jiri.vala11@gmail.com>
2021-05-06 08:50:51 +0200

    ✍️ Mistake in production config

--- admin/webpack.prod.js
+++ admin/webpack.prod.js
@@ -75,7 +75,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './src/photos/upload.html',
 			inject: true,
-			chunks: ['main', 'dash_upload'],
+			chunks: ['main', 'dashboard','dash_upload'],
 			filename: 'photos/upload.html'
 		}),
 	]
