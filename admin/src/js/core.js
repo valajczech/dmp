403e5cfc7226bd5a26143c744275bf0345d04b6f
valajczech <jiri.vala11@gmail.com>
2021-05-27 22:26:03 +0200

    Creating new albums

--- admin/src/js/core.js
+++ admin/src/js/core.js
@@ -1,9 +1,16 @@
 // Main (core) functions
+// Collection list is fetched from Firebase and then loaded into
+// collectionList Array.
+// `updateCollectionList` updated Firebase list as well as local
 
+// Variables
+let collectionList = [];
 export class ImageManipulations {
   static getCollectionsList() {
    // Get existing collections from Firebase Firestore
-   // Dummy albums for now
-    return ["default","album1",,"album2", "album3"];
+    return collectionList;
+  }
+  static updateColectionList(newAlbumName) {
+    collectionList.push(newAlbumName);
   }
 }
