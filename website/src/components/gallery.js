0d463f17999dff78db582c374d6e63fb00da9040
valajczech <jiri.vala11@gmail.com>
2021-12-22 18:36:35 +0100

    gallery component init

--- website/src/components/gallery.js
+++ website/src/components/gallery.js
@@ -3,10 +3,9 @@
 // Imports
 import { async } from "regenerator-runtime";
 import "../css/components/gallery.css";
+
+// Helpers
 import { Images } from "../js/core";
-
-// Variables
-
 
 export class Gallery extends HTMLElement {
   /**
@@ -15,40 +14,45 @@ export class Gallery extends HTMLElement {
 
   constructor(imageArray) {
     super();
-    this._imageArray = imageArray;
+    this._rawImageArray = imageArray;
+    this.currentIndex = 0;
+    this.total = imageArray.length;
   }
-  connectedCallback() {
-    console.log("imgLIST:",this._imageArray);
-     // Render the basic layout
-      this.innerHTML = `
-    <div class="gallery-thumbnail">
-      <img src=${this._imageArray[0]} id="gallery-item-thumbnail"/>
-    </div>
-    <div class="gallery-wrapper hidden">
-      <div class="gallery-top-controls">
-        <button id="gallery_close"><span class="typcn typcn-times"></span></button>
-      </div>
-      <div class="gallery-visible-item">
-        <div class="gallery-image-wrapper">
-          
-        </div>
-        <div class="gallery-image-controls">
-          <button id="gallery-prev-image" class="gallery-control"><span class="typcn typcn-arrow-left-thick"></span></button>
-          <button id="gallery-like-image" class="gallery-control"><span class="typcn typcn-heart-outline"></span></button>
-          <button id="gallery-next-image" class="gallery-control"><span class="typcn typcn-arrow-right-thick"></span></button>
-        </div>
-      </div>
-      <div class="gallery-image-reel">
-        <!-- Image items auto injected -->
-      </div>
-    </div>
-    `;
-    //! DOM Variables
-  }
-  //! Functions
-  previousImage() {}
-  nextImage() {}
-  async toggleLike(imageObject) {}
-}
 
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="gallery">
+  <div class="image-wrapper">
+    <div class="control" id="prev">
+      <span class="typcn typcn-chevron-left" id="arrow"></span>
+    </div>
+    <div class="images">
+    ${
+      this._rawImageArray.length == 0
+        ? `<p>No photos yet.</p>`
+        : `<img
+    src=${this._rawImageArray[this.currentIndex].imageSrc}
+    height="400"
+    alt=""
+    class="gallery-image"
+  />`
+    }
+    </div>
+    <div class="control" id="next">
+      <span class="typcn typcn-chevron-right" id="arrow"></span>
+    </div>
+  </div>
+</div>
+    `;
+
+    this.querySelector("#prev").onclick = () => {
+      this.currentIndex - 1 < 0 ? "" : (this.currentIndex -= 1);
+      console.log(this.currentIndex);
+    };
+    this.querySelector("#next").onclick = () => {
+      this.currentIndex + 1 > this.total ? "" : (this.currentIndex += 1);
+      console.log(this.currentIndex);
+    };
+  }
+}
 customElements.define("image-gallery", Gallery);
--- website/src/js/collection.js
+++ website/src/js/collection.js
@@ -4,58 +4,13 @@
 // Imports
 import { async } from "regenerator-runtime";
 import "../css/collections.css";
-import { Collections, Images } from "./core";
+import { Collections, Images, Storage } from "./core";
 import { Gallery } from "../components/gallery";
 
 // Variables
-let desiredCollection = new URL(document.location).searchParams.get(
-  "collection"
-);
-let imageList = new Array();
+let collectionId = new URL(document.location).searchParams.get("collectionId");
+let collectionObject = Storage.getSpecific(collectionId);
+const galleryWrapper = document.querySelector("#gallery-wrapper");
 
-// DOM Variables
-const collectionNameDOM = document.querySelector(".collectionName");
-const galleryPreview = document.querySelector(".gallery");
-const galleryPreviewItem = document.querySelector(".gallery-preview-item");
-
-document.addEventListener("DOMContentLoaded", async () => {
-  if (!desiredCollection) {
-    // Illegal state
-    //! BUG: when relocating to index.html, menu collection adding doesnt work
-    window.location.replace("/");
-  } else {
-    galleryPreview.style.display = "flex";
-    // Fetch desired collection info and connectedImages
-    // and then add them to gallery
-    await Collections.getCollection(desiredCollection)
-      .catch((err) => {
-        console.error(err);
-        galleryPreview.style.display = "none";
-        return result;
-      })
-      
-      .then((result) => {
-        imageList = result;
-        // Set the name in DOM
-        collectionNameDOM.innerText = result.albumName;
-        result.connectedImages.forEach(async (doc) => {
-          imageList.push(await Images.getImage(doc.imgDocID));
-        });
-
-        // Generate previewImage, onclick open LighGallery
-        if (result.itemCount < 1) {
-          // There are no connected Images
-          galleryPreview.innerHTML = "<h3>There are no images yet!</h3>";
-        }
-      })
-      .then(() => {
-        console.log("wat: ", imageList);
-        // Create and append gallery to the DOM using Gallery component
-        galleryPreview.appendChild(new Gallery(imageList));
-      });
-  }
-});
-
-/*
-
-*/
+document.querySelector(".collectionName").innerText = collectionObject.name;
+galleryWrapper.appendChild(new Gallery(collectionObject.images));
