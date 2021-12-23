f6dcd51c8e6f4be3d5d57069b2a03a5a6e92402d
valajczech <jiri.vala11@gmail.com>
2021-12-24 00:41:07 +0100

    Gallery swithcing items

--- website/src/components/gallery.js
+++ website/src/components/gallery.js
@@ -16,7 +16,7 @@ export class Gallery extends HTMLElement {
     super();
     this._rawImageArray = imageArray;
     this.currentIndex = 0;
-    this.total = imageArray.length;
+    this.total = imageArray.length - 1;
   }
 
   connectedCallback() {
@@ -27,31 +27,54 @@ export class Gallery extends HTMLElement {
       <span class="typcn typcn-chevron-left" id="arrow"></span>
     </div>
     <div class="images">
-    ${
-      this._rawImageArray.length == 0
-        ? `<p>No photos yet.</p>`
-        : `<img
-    src=${this._rawImageArray[this.currentIndex].imageSrc}
-    height="400"
-    alt=""
-    class="gallery-image"
-  />`
-    }
+      ${
+        this._rawImageArray.length == 0
+          ? `<p>Prozatím žádné fotky</p>`
+          : `<img
+      class="gallery-image"  src=${
+        this._rawImageArray[this.currentIndex].imageSrc
+      } height="400px" />`
+      }
     </div>
     <div class="control" id="next">
       <span class="typcn typcn-chevron-right" id="arrow"></span>
     </div>
   </div>
-</div>
+  </div>
+
     `;
 
+    let image = this.querySelector(".gallery-image");
+    let imageWrapper = this.querySelector(".images");
+
+    console.log(this.total);
     this.querySelector("#prev").onclick = () => {
       this.currentIndex - 1 < 0 ? "" : (this.currentIndex -= 1);
-      console.log(this.currentIndex);
+      console.log(this.currentIndex, "of", this.total);
+
+      imageWrapper.classList.add("fade");
+      image.setAttribute(
+        "src",
+        this._rawImageArray[this.currentIndex].imageSrc
+      );
+      setTimeout(() => {
+        imageWrapper.classList.remove("fade");
+      }, 1000);
     };
     this.querySelector("#next").onclick = () => {
       this.currentIndex + 1 > this.total ? "" : (this.currentIndex += 1);
-      console.log(this.currentIndex);
+      console.log(this.currentIndex, "of", this.total);
+
+      imageWrapper.classList.add("fade");
+
+      image.setAttribute(
+        "src",
+        this._rawImageArray[this.currentIndex].imageSrc
+      );
+      setTimeout(() => {
+        imageWrapper.classList.remove("fade");
+        console.log("1");
+      }, 1000);
     };
   }
 }
